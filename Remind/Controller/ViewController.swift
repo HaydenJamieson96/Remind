44b3efe8ff179931106278fc3f60d4d48aedd603
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-15 15:22:22 +0000

    UI Configured
    App icons set up
    Buttons set up

--- Remind/Controller/ViewController.swift
+++ Remind/Controller/ViewController.swift
@@ -15,11 +15,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onTimerTapped(_ sender: Any) {
+        print("timer")
     }
-
-
+    
+    @IBAction func onDateTapped(_ sender: Any) {
+        print("date")
+    }
+    
+    @IBAction func onLocationTapped(_ sender: Any) {
+        print("location")
+    }
+    
+    
+    
+    
 }
 
---
+++ Remind/View/ShadowView.swift
@@ -0,0 +1,23 @@
+//
+//  ShadowView.swift
+//  Remind
+//
+//  Created by Hayden Jamieson on 15/01/2018.
+//  Copyright © 2018 Hayden Jamieson. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    
+    override func awakeFromNib() {
+        layer.shadowPath = CGPath(rect: layer.bounds, transform: nil)
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+        layer.shadowOpacity = 1
+        layer.shadowRadius = 5
+        
+        layer.cornerRadius = 5
+    }
+    
+}
